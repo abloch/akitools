e4925bb1d4dfb66fd6a55869bf45b4116cc803c6
Akiva Bloch <akiva@kubiya.ai>
2025-01-23 17:39:48 +0200

    w

---
+++ kuber/tools/main.py
@@ -0,0 +1,16 @@
+import inspect
+
+from kubiya_sdk.tools.models import Arg, Tool, FileSpec
+from kubiya_sdk.tools.registry import tool_registry
+
+import base
+
+go_tool = Tool(
+    name="go_version",
+    type="docker",
+    image="golang:1.22",
+    description="gets the go version",
+    content="""go version""",
+)
+
+tool_registry.register(go_tool)
