d762eb05ba0c7b2a96b674da3cddbbec3f6cfb74
Akiva Bloch <akiva@kubiya.ai>
2025-01-23 18:32:04 +0200

    w

--- kuber/tools/main.py
+++ kuber/tools/main.py
@@ -1,20 +1,14 @@
-from kubiya_sdk.tools import Arg, Tool
-
+from kubiya_sdk.tools import Tool
 from kubiya_sdk.tools.registry import tool_registry
-import sys
 
 KUBERNETES_ICON_URL = "https://kubernetes.io/icons/icon-128x128.png"
 
 
 gover = Tool(
-    name="gover",
-    description="checks go version.",
+    name="gover-private",
+    description="checks go version private repo.",
     image="564407622114.dkr.ecr.eu-west-1.amazonaws.com/kubiya-base:golang-1.21",
     content="""go version""",
 )
 
-try:
-    tool_registry.register("gover", gover)
-except Exception as e:
-    print(f"❌ Failed to register gover tool: {str(e)}", file=sys.stderr)
-    raise
+tool_registry.register("gover", gover)
