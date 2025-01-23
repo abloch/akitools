86f4c9b74e47443b800263cc78110f3b9c476ec0
Akiva Bloch <akiva@kubiya.ai>
2025-01-23 17:41:50 +0200

    w

--- kuber/tools/gover.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def get_go_version():
-    ret = subprocess.check_output(["go", "version"])
-    print(ret.decode("utf-8"))
-
-
-if __name__ == "__main__":
-    get_go_version()
