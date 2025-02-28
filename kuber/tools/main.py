dac8e9ccca7c4a0119d259c078b762bde3cc66b4
Akiva Bloch <akiva@kubiya.ai>
2025-02-28 08:54:36 +0200

    test

--- kuber/tools/main.py
+++ kuber/tools/main.py
@@ -5,7 +5,7 @@ KUBERNETES_ICON_URL = "https://storage.getlatka.com/images/kubiya.ai.png"
 
 
 onboarder = Tool(
-    name="onborader",
+    name="onborader2",
     description="onboard a new organization to kubiya",
     image="akiva/curler",
     content="""curl https://api.kubiya.ai/api/v1/onboard -H "Authorization: UserKey ${KUBIYA_API_KEY}" -d " $(jo org_name=${org_name} admin_email=${admin_email})" """,
