cab56e896a842dfabe8169a571bdc5e19a0ef2bf
Akiva Bloch <akiva@kubiya.ai>
2025-02-28 09:12:00 +0200

    test

--- kuber/tools/main.py
+++ kuber/tools/main.py
@@ -9,7 +9,7 @@ KUBERNETES_ICON_URL = "https://kubernetes.io/icons/icon-128x128.png"
 gover = Tool(
     name="gover",
     description="checks go version.",
-    image="golang:latest",
+    image="golang:latest1",
     content="""go version""",
 )
 
